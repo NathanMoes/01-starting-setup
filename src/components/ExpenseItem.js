d68763419cbbd93e191242a58fbc29f571c2277d
NathanMoes <nathanmoes7@gmail.com>
2022-12-17 12:51:22 -0800

    First Commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+function ExpenseItem(props) {
+    // const expenseDate = props.date;
+    const expenseTitle = props.title;
+    const expenseAmount = props.amount;
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
+        </div>
+    );
+}
+export default ExpenseItem;
