8606f4028529e315052baca9ebfb32b1d3f56d1c
NathanMoes <nathanmoes7@gmail.com>
2022-12-17 14:37:59 -0800

    State managment test Expense item

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+
+function Expenses(props) {
+    let array = []
+    for (let i = 0; i < props.num; i += 1) {
+        array.push(<ExpenseItem date={props.expense[i].date} title={props.expense[i].title} amount={props.expense[i].amount} />);
+    }
+    return (
+        <Card className='expenses'>{array}</Card>
+    )
+}
+
+export default Expenses;
