4d9c0d117063ab20cf3f38cb36312ad80e959864
NathanMoes <nathanmoes7@gmail.com>
2022-12-18 11:09:25 -0800

    State Component for filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
     let array = []
     for (let i = 0; i < props.num; i += 1) {
         array.push(<ExpenseItem date={props.expense[i].date} title={props.expense[i].title} amount={props.expense[i].amount} />);
     }
+    const [expenses, setFilteredExpenses] = useState(array);
+    const [yearFilterBy, setYearFilterby] = useState('');
+    const changeYear = (newYear) => {
+        setYearFilterby(newYear);
+        console.log(newYear);
+    }
     return (
-        <Card className='expenses'>{array}</Card>
+        <>
+            <Card className='expenses'><ExpensesFilter onToggle={changeYear} />{expenses}</Card>
+        </>
     )
 }
 
